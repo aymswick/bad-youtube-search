e0bfbe60d941916fbb87949d437e2b86e0585805
aymswick <asymkowick@gmail.com>
2016-04-18 15:01:38 -0700

    Restructure, button works now

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 #Bad Youtube Search - Python 3
 from tkinter import *
 
-class Application(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
 
-    def createWidgets(self):
-        self.goButton = Button(self, fg="red", bg="blue")
-        self.goButton["text"] = "Huh?"
-        self.goButton["command"] = self.yell
-        self.goButton.pack(side="top")
 
-        self.QUIT = Button(self, text="QUIT", fg="red",
-                           command=app.destroy)
-        self.QUIT.pack(side="bottom")
-
-    def yell(self):
-        print("AHHHHHHHHHHHHHHHHH")
+def onClick():
+    print("Hello")
 
 #Run this on application create
 app = Tk()
 app.title("Bad Youtube Search")
 app.geometry("600x400")
 app.configure(bg="#8f8f8f")
+
+#Send Button
+sendButton = Button(app, font="Helvetica", text="SEND", width="50", height=5,
+                    bd=0, bg="#BDE096", activebackground="#BDE096", justify="center",
+                    command=onClick)
+
+sendButton.place(x=255, y=360, height=40, width=130)
+
 app.mainloop()
